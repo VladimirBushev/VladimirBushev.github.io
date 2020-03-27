86264863a8b96b695af8499bc25afcdd5d00f937
VladimirBushev <62628351+VladimirBushev@users.noreply.github.com>
2020-03-27 23:37:04 +0300

    powerupdate

--- slider/slider.js
+++ slider/slider.js
@@ -1,8 +1,8 @@
 var gallery = ['https://avatars.mds.yandex.net/get-pdb/245485/7e207740-b9fb-4241-94bb-30b108e36130/s1200 ',
     'https://avatars.mds.yandex.net/get-pdb/1539674/523600ba-3216-4475-aeef-03f30802f036/s1200?webp=false',
     'https://avatars.mds.yandex.net/get-pdb/918543/727f58ae-4e71-4446-a1b1-19adce0eb6b7/s1200?webp=false',
-    'https://a.d-cd.net/2YAAAgKeqOA-1920.jpg']
-    //'https://4.bp.blogspot.com/-PRPBBXysGyA/UcXMmaRRzGI/AAAAAAAAFi8/dC_wR-DxgWY/s1600/b0168_subaru_impreza_1_gen_wrx_sti_22.jpg']
+    'https://a.d-cd.net/2YAAAgKeqOA-1920.jpg',
+    'https://4.bp.blogspot.com/-PRPBBXysGyA/UcXMmaRRzGI/AAAAAAAAFi8/dC_wR-DxgWY/s1600/b0168_subaru_impreza_1_gen_wrx_sti_22.jpg']
 
 var img = document.getElementById('img');
 var rb = document.getElementById('right-button');
