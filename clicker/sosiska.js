9dfe4a22aa624c0df49a5cf34bb6dc3b18c62345
VladimirBushev <62628351+VladimirBushev@users.noreply.github.com>
2020-03-28 01:55:30 +0300

    ы

--- clicker/sosiska.js
+++ clicker/sosiska.js
@@ -1,9 +1,32 @@
+var img = document.getElementById('masha');
+var img2 = document.getElementById('katya');
+var title = document.getElementById('title');
+
+function clickedMasha(){
+    title.className = 'hidden-img';
+    img2.className = 'hidden-img';
+    //img.style.width = '300px';
+    img.src = 'https://sun9-30.userapi.com/c857320/v857320058/f75e0/_bry66ZYpCM.jpg'
+}
+
+function clickedKatya(){
+    title.className = 'hidden-img';
+    img.className = 'hidden-img';
+    img2.src = 'https://sc02.alicdn.com/kf/HTB1ppZERpXXXXXpXFXXq6xXFXXXI/Plus-Size-women-high-heeled-shoes-footwear.jpg';
+}
+
 function inputErrorId(id){
     var element = document.getElementById(id);
     element.className = 'errorInput';
 }
 
 function allInputError(){
+    title.className = 'display-img';
+    img.className = 'display-img';
+    img2.className = 'display-img';
+    img.src = 'https://sun9-30.userapi.com/c636930/v636930603/5a0aa/zEA6AiUGtgs.jpg';
+    img2.src = 'https://sun9-71.userapi.com/c856120/v856120331/33b72/CCdUDWriJWg.jpg';
+    img.style.width = '750px';
     inputErrorId('sosiska');
     inputErrorId('sosisochka');
     inputErrorId('sardelka');
@@ -12,5 +35,5 @@ function allInputError(){
 var sendButton = document.getElementById('button');
 sendButton.addEventListener('click', allInputError);
 
-var sosiska = document.getElementById('sosiska');
-sosiska.addEventListener('focus', allInputError);
+img.addEventListener('click', clickedMasha);
+img2.addEventListener('click', clickedKatya);
