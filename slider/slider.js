3f35cb21acd0c7451cdb41eadec7ff84144e1268
VladimirBushev <62628351+VladimirBushev@users.noreply.github.com>
2020-03-30 04:23:02 +0300

    s

--- slider/slider.js
+++ slider/slider.js
@@ -55,6 +55,8 @@ function SliderDesigner(name) {
 
 var slider1 = new SliderDesigner('slider1');
 var slider2 = new SliderDesigner('slider2');
+var slider3 = new SliderDesigner('slider3');
 
 slider1.start();
 slider2.start();
+slider3.start();
