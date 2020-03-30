5a6ee88d7583bb6f219f9bbaac7d8440955e74fa
VladimirBushev <62628351+VladimirBushev@users.noreply.github.com>
2020-03-30 04:13:26 +0300

    h

--- slider/index.js
+++
@@ -1,6 +0,0 @@
-var slider1 = sliderFactory.createNewSlider(); 
-var slider2 = sliderFactory.createNewSlider(); 
-
-slider1.start('slider1');
-slider2.start('slider2');
-    
--- slider/slider.js
+++ slider/slider.js
@@ -1,58 +1,60 @@
-var sliderFactory = {
-    createNewSlider: function () {
-        var Newslider = {
-            gallery: [],
-            img: null, // document.getElementById('img'),
-            rb: null, //document.getElementById('right-button'),
-            lb: null, //document.getElementById('left-button'),
-            i: 0,
+function SliderDesigner(name) { 
+    this.gallery = [];
+    this.img = null;
+    this.rb = null;
+    this.lb = null;
+    this.i = 0;
+    this.elId = name;
 
-            start: function (elId) {
-                var that = this;
+    this.start = function () {
+        var that = this;
 
-                var el = document.querySelector('#' + elId);
+        var el = document.querySelector('#' + this.elId);
 
-                this.rb = el.querySelector('.right');
-                this.lb = el.querySelector('.left');
-                this.img = el.querySelector('.img');
+        this.rb = el.querySelector('.right');
+        this.lb = el.querySelector('.left');
+        this.img = el.querySelector('.img');
 
-                this.gallery.push('https://avatars.mds.yandex.net/get-pdb/245485/7e207740-b9fb-4241-94bb-30b108e36130/s1200 ');
-                this.gallery.push('https://i.ytimg.com/vi/7DzGwki9eyU/maxresdefault.jpg');
-                this.gallery.push('https://w.wallhaven.cc/full/p8/wallhaven-p8l3d9.jpg');
-                this.gallery.push('https://delessencedansmesveines.com/wp-content/uploads/2017/02/DLEDMV-Mitsu-Lancer-Evo-5-JDM-11.jpg');
-                this.gallery.push('https://cdn.motor1.com/images/mgl/kmgJR/s3/1998-subaru-impreza-22b-sti.jpg');
+        this.gallery.push('https://avatars.mds.yandex.net/get-pdb/245485/7e207740-b9fb-4241-94bb-30b108e36130/s1200 ');
+        this.gallery.push('https://i.ytimg.com/vi/7DzGwki9eyU/maxresdefault.jpg');
+        this.gallery.push('https://w.wallhaven.cc/full/p8/wallhaven-p8l3d9.jpg');
+        this.gallery.push('https://delessencedansmesveines.com/wp-content/uploads/2017/02/DLEDMV-Mitsu-Lancer-Evo-5-JDM-11.jpg');
+        this.gallery.push('https://cdn.motor1.com/images/mgl/kmgJR/s3/1998-subaru-impreza-22b-sti.jpg');
 
-                this.img.src = this.gallery[this.i];
+        this.img.src = this.gallery[this.i];
 
-                this.rb.addEventListener('click', function (e) {
-                    that.right();
-                });
+        this.rb.addEventListener('click', function (e) {
+            that.right();
+        });
 
-                this.lb.addEventListener('click', function (e) {
-                    that.left();
-                });
-            },
+        this.lb.addEventListener('click', function (e) {
+            that.left();
+        });
+    };
 
-            right: function (e) {
-                if (this.i === (this.gallery.length - 1)) {
-                    this.i = 0;
-                }
-                else {
-                    this.i++;
-                }
-                this.img.src = this.gallery[this.i]
-            },
-
-            left: function (e) {
-                if (this.i === 0) {
-                    this.i = (this.gallery.length - 1);
-                }
-                else {
-                    this.i--;
-                }
-                this.img.src = this.gallery[this.i]
-            }
+    this.right = function (e) {
+        if (this.i === (this.gallery.length - 1)) {
+            this.i = 0;
         }
-        return Newslider;
-    }
+        else {
+            this.i++;
+        }
+        this.img.src = this.gallery[this.i]
+    };
+
+    this.left = function (e) {
+        if (this.i === 0) {
+            this.i = (this.gallery.length - 1);
+        }
+        else {
+            this.i--;
+        }
+        this.img.src = this.gallery[this.i]
+    };
 }
+
+var slider1 = new SliderDesigner('slider1');
+var slider2 = new SliderDesigner('slider2');
+
+slider1.start();
+slider2.start();
